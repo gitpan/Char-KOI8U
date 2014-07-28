a518aedb54cafabfe2bcef1dc3e2edb5d65fdf15
INABA Hitoshi <ina@cpan.org>
2014-07-28 16:25:48 +0000

    Import of INA/Char-KOI8U-0.97 from CPAN.
    gitpan-cpan-distribution: Char-KOI8U
    gitpan-cpan-version:      0.97
    gitpan-cpan-path:         INA/Char/KOI8U/Char-KOI8U-0.97.tar.gz
    gitpan-cpan-author:       INA
    gitpan-cpan-maturity:     released

--- Char/Ekoi8u.pm
+++ Char/Ekoi8u.pm
@@ -3,7 +3,7 @@ package Char::Ekoi8u;
 #
 # Char::Ekoi8u - Run-time routines for Char/KOI8U.pm
 #
-# http://search.cpan.org/dist/Char-Char::KOI8U/
+# http://search.cpan.org/dist/Char-KOI8U/
 #
 # Copyright (c) 2008, 2009, 2010, 2011, 2012, 2013, 2014 INABA Hitoshi <ina@cpan.org>
 ######################################################################
@@ -17,7 +17,7 @@ use 5.00503;    # Galapagos Consensus 1998 for primetools
 # (and so on)
 
 BEGIN { eval q{ use vars qw($VERSION) } }
-$VERSION = sprintf '%d.%02d', q$Revision: 0.96 $ =~ /(\d+)/xmsg;
+$VERSION = sprintf '%d.%02d', q$Revision: 0.97 $ =~ /(\d+)/xmsg;
 
 BEGIN {
     if ($^X =~ / jperl /oxmsi) {
